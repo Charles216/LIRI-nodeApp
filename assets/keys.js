1d3cca05bc9bcb8fc00e158a98e628502aab2c6d
Elephant_Man <grogan.charlie@gmail.com>
2018-08-29 21:53:38 -0500

    spotify and OMDB both return required responses

--- assets/keys.js
+++ assets/keys.js
@@ -1,14 +1,13 @@
-console.log('this is loaded');
 // export
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
 
-exports.OMDB = {
- id: process.env.API_KEY,
-};
+//exports.OMDB = {
+// id: process.env.API_KEY,
+//};
 
-exports.BiT = {
-  //api info goes heres
-};
+/* exports.BiT = {
+  id: process.env.BAND_KEY,
+}; */
--- assets/liri.js
+++ assets/liri.js
@@ -1,6 +1,5 @@
 let dotenv = require("dotenv").config();
 let keys = require("./keys.js");
-//console.log(keys);
 let request = require('request');
 let Spotify = require('node-spotify-api');
 let userInput = process.argv[2];
@@ -13,14 +12,14 @@ switch (userInput) {
         break;
 
     case 'band':
-        concertInfo(artist);
+        concertInfo(searchAction);
         break;
 
     default: 'movie';
-        movieInfo('title');
+        movieInfo(searchAction);
         break;
 }
-
+ 
 // Spotify Function
 function spot(song) {
 
@@ -32,52 +31,65 @@ function spot(song) {
             return console.log('Error occurred: ' + err);
         }
 
-         //console.log(data.tracks.items[0].album.name)
+        //console.log(data.tracks.items[0].album.name)
         for (var i = 0; i < data.tracks.items.length; i++) {
             var songsInfo = data.tracks.items[i];
-            
-            console.log(songsInfo);
+
+            //console.log(songsInfo);
 
             console.log("Artist Name: " + songsInfo.album.artists[0].name);
             console.log("Song Name: " + songsInfo.track);
             console.log("Album Name: " + songsInfo.album.name);
-            console.log(); 
-                    
+            //console.log(); 
+
         }
-
-
     });
 }
-
+ 
 // Movie Function
-function movieInfo() {
+function movieInfo(title) {
     let urlOMDB =
-        "http://www.omdbapi.com/?t=" + "snatch" + "&y=&plot=full&tomatoes=true&apikey=" + + keys.OMDB.API_KEY;
+        "http://www.omdbapi.com/?t=" + title + "&y=&plot=full&tomatoes=true&apikey=trilogy";
 
     request(urlOMDB, function (error, response, body) {
+        let info = JSON.parse(body);
+        let title = info.Title;
+        let year = info.Year;
+        let rating = info.Rated;
+        let cast = info.Actors;
+        let plot = info.Plot;
+        let lang = info.Language;
+        let origin = info.Country;
+        let IMDBrate = info.imdbRating;
+        let rotTom = info.Ratings[2].Value;
+        
+        let movieResponse = `The movie ${title} came out in ${year}. ${title}, had a star studded cast consisting ${cast}.
+        
+The plot film goes a little something like this... ${plot} 
+        
+${title}, is available in ${lang} language(s). The film's country of origin is ${origin}. 
 
-        console.log('error:', error); // Print the error if one occurred
+The film recieved an IMDB User Rating of: ${IMDBrate} and a Rotten Tomates Rating of: ${rotTom}`;
 
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        if (!error) {
+            console.log(`${movieResponse}`);
+        } else {
+            console.log(error)
+        }
 
-        console.log('body:', body); // Print the HTML for the Google homepage.
+       
     });
 };
-// movieInfo();
-
+ 
 // Bands in Town Function
-function concertInfo() {
+function concertInfo(artist) {
     let urlBand =
-        "http://rest.bandsintown.com" //apiKey;
+        "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
     request(urlBand, function (error, response, body) {
-
-        console.log('error:', error); // Print the error if one occurred
-
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-
-        console.log('body:', body); // Print the HTML for the Google homepage.
+        let musicInfo = JSON.stringify(body);
+        console.log(musicInfo);
     });
 };
-// concertInfo();
+
 
